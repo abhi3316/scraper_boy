2dbe991954e7bdfeed3cdf0602cc01ceec94e6b5
abhi <abhijith.uwc.rw@gmail.com>
2024-10-26 18:01:34 +0530

    added notice to the frontend

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup 
-#import notice
+import notice
 import news
+import signal
+import os 
+
+notice_url = "https://www.canada.ca/en/immigration-refugees-citizenship/news/notices.html"
 
 news_url = "https://www.canada.ca/en/news/advanced-news-search/news-results.html?typ=newsreleases&dprtmnt=departmentofcitizenshipandimmigration&start=2015-01-01&end="
 speech_url = "https://www.canada.ca/en/news/advanced-news-search/news-results.html?typ=speeches&dprtmnt=departmentofcitizenshipandimmigration&start=2015-01-01&end="
@@ -12,32 +16,47 @@ background_url = "https://www.canada.ca/en/news/advanced-news-search/news-result
 
 media_advisories = "https://www.canada.ca/en/news/advanced-news-search/news-results.html?typ=mediaadvisories&dprtmnt=departmentofcitizenshipandimmigration&start=2015-01-01&end="
 
+year = 2024
+
+def handle_sigterm(signum, frame):
+    print("Received SIGTERM, shutting down gracefully.")
+    # Perform cleanup here
+    exit(0)
+
 
 def main():
 	print("Canada News Scrapping platform")
-	while 1:
-		print("1: News")
-		print("2: Notice")
-		inp = int(input("inp:"))
-		if(inp == 1):
+	signal.signal(signal.SIGTERM, handle_sigterm)
+	try:
+		while 1:
 			print("1: News")
-			print("2: Statement")
-			print("3: Background")
-			print("4: Media")
-			print("5: Speech")
-			news_inp = int(input("inp:"))
-			if(news_inp == 1):
-				news.news_get(news_url)
-			if(news_inp == 2):
-				news.news_get(statement_url)
-			if(news_inp == 3):
-				news.news_get(background_url)
-			if(news_inp == 4):
-				news.news_get(media_advisories)
-			if(news_inp == 5):
-				news.news_get(speech_url)
-			else:
-				print("Input wrong")	
+			print("2: Notice")
+			inp = int(input("inp:"))
+			if(inp == 1):
+				print("1: News")
+				print("2: Statement")
+				print("3: Background")
+				print("4: Media")
+				print("5: Speech")
+				news_inp = int(input("inp:"))
+				if(news_inp == 1):
+					news.news_get(news_url)
+				if(news_inp == 2):
+					news.news_get(statement_url)
+				if(news_inp == 3):
+					news.news_get(background_url)
+				if(news_inp == 4):
+					news.news_get(media_advisories)
+				if(news_inp == 5):
+					news.news_get(speech_url)
+				else:
+					print("Input wrong")	
+
+			if(inp == 2):
+				notice.notice_get(notice_url, year)
+	except:
+		print("\nClosing the program...")
+		exit(0)
 
 if __name__ == "__main__":
 	main()
